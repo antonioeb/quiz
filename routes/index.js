24b8f4083d91ce2b2c0382411ee0acdeb952ab68
antonio <a.espin.ballesta@gmail.com>
2015-06-06 14:30:36 +0100

    Creditos

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,12 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+
+/* GET a la pagina de creditos  */
+router.get('/author', function(req, res) {
+  res.render('quizes/author', {});
+});
+
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
 
