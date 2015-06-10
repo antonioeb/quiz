c060afd457e2a865bfa7a68bb26ab602bab850d6
antonio <a.espin.ballesta@gmail.com>
2015-06-10 19:16:31 +0100

    Autoload

--- routes/index.js
+++ routes/index.js
@@ -14,6 +14,9 @@ router.get('/author', function(req, res) {
   res.render('quizes/author', {});
 });
 
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load); // autoload :quizId
+
 // Definición de rutas de /quizes
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
